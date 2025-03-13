5b5190896af9aeaea85fe8038692c5a931a96973
MatheusBortolo <31974265+Matheusbortolo@users.noreply.github.com>
2025-03-13 11:52:57 -0300

    Feat: Criando todas as rotas. definindo as post para cadastrar, as get como retorno da lista completa ou de um unico produto

--- main.py
+++ main.py
@@ -1,10 +1,47 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from typing import Optional
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"message": "Olá, Mundo!"}
+
+@app.get("/feriados/")
+def read_feriado(id: Optional[int] = None):
+    return {"message": "feriados!"}
+
+@app.post("/feriados")
+async def set_feriado(request: Request):
+    body = await request.json()
+    return {"message": "feriados!"}
+
+@app.get("/clientes/")
+def read_clientes(id: Optional[int] = None):
+    return {"message": "clientes!"}
+
+@app.post("/clientes")
+async def set_cliente(request: Request):
+    body = await request.json()
+    return {"message": "clientes!"}
+
+@app.get("/tipo-servico/")
+def read_tipo_servico(id: Optional[int] = None):
+    return {"message": "tipo-servico!"}
+
+@app.post("/tipo-servico")
+async def set_tipo_servico(request: Request):
+    body = await request.json()
+    return {"message": "tipo-servico!"}
+
+@app.get("/agenda/")
+def read_agenda(id: Optional[int] = None): 
+    return {"message": "agenda!"}
+
+@app.post("/agenda")
+async def set_agenda(request: Request):
+    body = await request.json()
+    return {"message": "agenda!"}
 
 if __name__ == "__main__":
     import uvicorn
